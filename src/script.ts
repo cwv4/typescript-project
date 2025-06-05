f54f4d72cc666c9be41f184711d1ec241abfda61
Caleb Warner <cwarner@vertafore.com>
2025-06-05 17:23:17 -0400

    Added interfaces

--- src/script.ts
+++ src/script.ts
@@ -2,3 +2,31 @@ let x = 123;
 let y: string;
 let z: boolean;
 let a: Date;
+
+type Description = string;
+
+interface Tree {
+    height: number;
+    age: number;
+    description?: Description;
+};
+
+interface PalmTree extends Tree {
+    numPalmLeaves: number;
+};
+
+let myTree: Tree = {
+    height: 10,
+    age: 5,
+    description: "Small oak tree"
+};
+
+let myPalm: PalmTree = {
+    height: 15,
+    age: 7,
+    numPalmLeaves: 70
+};
+
+console.log("My regular tree is " + myTree.height + " feet tall and " + myTree.age + " years old.");
+console.log("My palm tree is " + myPalm.height + " feet tall and " + myPalm.age + " years old and it has " + myPalm.numPalmLeaves + " leaves.")
+
